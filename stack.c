9586ace1b2cacdf9725958465ebb877250d4cea1
HyperWinX <artmsmth@gmail.com>
2023-12-08 18:51:46 +0400

    Updated smemcpy function, removed some debug features. Final commit.

--- stack.c
+++ stack.c
@@ -15,57 +15,59 @@ void dmp(struct Stack* stack){
 }
 
 void smemcpy(char* dst, char* src, size_t n){
-    while(n--) *dst++ = *src++;
+	if (n % 4 != 0)
+		while(n--) *dst++ = *src++;
+	else
+		while(n -= 4) *(int*)dst++ = *(int*)src++;
 } 
 
-void stack_lifo_new(struct Stack* stack, size_t initial_size){
+int stack_lifo_new(struct Stack* stack, size_t initial_size){
 	void* ptr = calloc(sizeof(char), initial_size);
-	printf("Initial size: %zu\n", initial_size);
-	if (!ptr) return;
+	if (!ptr) return -1;
 	stack->sptr = ptr + initial_size;
 	stack->bptr = stack->sptr;
 	stack->srcptr = ptr;
 	stack->size = 0;
 	stack->allocated = initial_size;
 	stack->type = LIFO;
+	return 0;
 }
 
-void stack_fifo_new(struct Stack* stack, size_t initial_size){                                                   void* ptr = calloc(sizeof(char), initial_size);                                                     if (!ptr) return;
-    stack->sptr = ptr + initial_size;
-    stack->bptr = ptr + initial_size;
-    stack->srcptr = ptr;
-    stack->size = 0;                                  
-    stack->allocated = initial_size;
-    stack->type = FIFO;                      
+int stack_fifo_new(struct Stack* stack, size_t initial_size){
+	void* ptr = calloc(sizeof(char), initial_size);
+	if (!ptr) return -1;
+	stack->sptr = ptr + initial_size;
+	stack->bptr = ptr + initial_size;
+	stack->srcptr = ptr;
+	stack->size = 0;                         
+	stack->allocated = initial_size;
+	stack->type = FIFO;
+	return 0;
 }
 
 int stack_push(struct Stack* stack, void* element, size_t elementsize){
 	if (stack->allocated - stack->size < elementsize) return -1;
-	//dmp(stack);
-    stack->bptr -= elementsize;
-    memcpy(stack->bptr, element, elementsize);
-    stack->size += elementsize;
-    //dmp(stack);
-    return 0;
+	stack->bptr -= elementsize;
+	memcpy(stack->bptr, element, elementsize);
+	stack->size += elementsize;
+	return 0;
 }
 
 int stack_pop(struct Stack* stack, void* element, size_t elementsize){
 	if (stack->size < elementsize) return -1;
 	if (stack->type == LIFO){
-		//dmp(stack);
-		memcpy(element, stack->bptr, elementsize);        
-        stack->bptr += elementsize;                       
-        stack->size -= elementsize;
-	//dmp(stack);
-        return 0;
+		memcpy(element, stack->bptr, elementsize);
+        	stack->bptr += elementsize;
+        	stack->size -= elementsize;
+        	return 0;
 	}
-    else if (stack->type == FIFO){
-        memcpy(element, stack->sptr - elementsize, elementsize);
-        stack->size -= elementsize;
-	stack->bptr += elementsize;
-        smemcpy((char*)stack->bptr, (char*)stack->bptr - elementsize, stack->size);
-	return 0;
-    } 
+    	else if (stack->type == FIFO){
+        	memcpy(element, stack->sptr - elementsize, elementsize);
+        	stack->size -= elementsize;
+		stack->bptr += elementsize;
+        	smemcpy((char*)stack->bptr, (char*)stack->bptr - elementsize, stack->size);
+		return 0;
+    	} 
 	return -1;
 }
 
--- stack.h
+++ stack.h
@@ -4,7 +4,7 @@ struct Stack{                                             void* sptr;           
 	void* srcptr;
         size_t size;                                      size_t allocated;                                 int16_t type;                             };
 
-void stack_lifo_new(struct Stack* stack, size_t initial_size);
-void stack_fifo_new(struct Stack* stack, size_t initial_size);
+int stack_lifo_new(struct Stack* stack, size_t initial_size);
+int stack_fifo_new(struct Stack* stack, size_t initial_size);
 int stack_push(struct Stack* stack, void* element, size_t elementsize);
 int stack_pop(struct Stack* stack, void* element, size_t elementsize);
